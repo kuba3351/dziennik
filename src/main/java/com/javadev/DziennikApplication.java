ebd05d0c35d102c8fc3799d1ae7e89900ed6a437
kuba3351 <kuba3351@gmail.com>
2016-04-25 15:15:48 +0200

    initial

---
+++ src/main/java/com/javadev/DziennikApplication.java
@@ -0,0 +1,12 @@
+package com.javadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DziennikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DziennikApplication.class, args);
+	}
+}
